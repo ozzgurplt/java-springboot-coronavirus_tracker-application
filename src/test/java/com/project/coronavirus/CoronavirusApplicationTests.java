b853efd2a108362bc0a56a4b5910ede6bbeedb8a
ozzgurplt <alacahan785@gmail.com>
2021-08-17 15:51:54 +0300

    Initial commit

---
+++ src/test/java/com/project/coronavirus/CoronavirusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coronavirus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
